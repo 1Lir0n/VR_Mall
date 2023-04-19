f1af6740a46e0859cef1b68034e1ae19431b4a6a
1Lir0n <lironazoulay51@gmail.com>
2023-04-20 02:12:10 +0300

    Failed animation starter on cash register

---
+++ Assets/scripts/OpenCashRegister.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OpenCashRegister : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Translate(-0.04f,0,0);
+    }
+    private void OnTriggerEnter()
+    {
+        
+    }
+}
